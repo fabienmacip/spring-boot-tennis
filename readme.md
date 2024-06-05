434705023e79e7613ef1470fed80f8d184292f7a
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-06-05 12:42:39 +0200

    Creation d'un premier repository - not working...

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ Pour initialiser, ajouter ces 2 lignes dans application.properties :
 
 spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 server.port=8084
+
--- src/main/java/com/webfm/tennis/service/HealthCheckService.java
+++ src/main/java/com/webfm/tennis/service/HealthCheckService.java
@@ -2,12 +2,26 @@ package com.webfm.tennis.service;
 
 import com.webfm.tennis.ApplicationStatus;
 import com.webfm.tennis.HealthCheck;
+import com.webfm.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
+
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthcheck(){
-        return new HealthCheck(ApplicationStatus.OK, "Welcome to Webfm Tennis !");
+        Long activeSessions = healthCheckRepository.countApplicationConnections();
+
+        if(activeSessions > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "Welcome to Webfm Tennis !");
+        } else {
+            return new HealthCheck(ApplicationStatus.KO, "Webfm Tennis is not fully functional, please check your configuration.");
+        }
+
+        //return new HealthCheck(ApplicationStatus.OK, "Welcome to Webfm Tennis !");
     }
 
 }
