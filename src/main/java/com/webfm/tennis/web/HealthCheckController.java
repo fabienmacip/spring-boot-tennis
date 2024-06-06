1eb69afa18b31643aa568353a5ee3470b5820b0f
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2024-06-06 11:25:48 +0200

    Mise en place interface de démo OpenAPI

--- src/main/java/com/webfm/tennis/web/HealthCheckController.java
+++ src/main/java/com/webfm/tennis/web/HealthCheckController.java
@@ -2,15 +2,32 @@ package com.webfm.tennis.web;
 
 import com.webfm.tennis.HealthCheck;
 import com.webfm.tennis.service.HealthCheckService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "HealthCheck API")
 @RestController
 public class HealthCheckController {
 
     @Autowired
     private HealthCheckService healthCheckService;
+
+    @Operation(summary = "Returns application status", description = "Returns the application status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Healthcheck status with some details",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HealthCheck.class))})
+
+    })
 
     @GetMapping("/healthcheck")
     public HealthCheck healthcheck(){
